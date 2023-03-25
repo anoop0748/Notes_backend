3cfc81e2298f9a53c7cbc12dbb669884e7e7ce79
Anoop Singh <anoop.gada@gmail.com>
2023-03-25 14:02:56 +0530

    Allow all origin

--- app.js
+++ app.js
@@ -10,6 +10,14 @@ const login = require('./src/login')
 const app = express();
 app.use(express.json());
 
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization",
+  );
+  next();
+});
 mongoose.connect(mongoDBURL,console.log("Connected To Database"));
 app.use(body_parser.json());
 app.use('/login/user/*', async (req, res, next) => {
